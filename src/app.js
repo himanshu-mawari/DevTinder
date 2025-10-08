74a0013df27c6029855c2da36a563bd87284205f
himanshu-mawari <mawrihimanshu83@gmail.com>
2025-10-08 22:02:46 +0530

    exploring regex-based route handlers

--- src/app.js
+++ src/app.js
@@ -1,25 +1,31 @@
 const express = require("express");
 const app = express();
 
-app.get("/user" , (req , res) =>{
-    res.send("Hello Himanshu!!!");
-});
-
-app.post("/user" , (req , res) =>{
-    res.send("Got a post request !!!")
-})
-app.put("/user" , (req , res) =>{
-    res.send("Got a put request at /user !!!")
-})
-app.patch("/user" , (req , res) =>{
-    res.send("Got a patch request at /user !!!")
-})
-app.delete("/user" , (req , res) =>{
-    res.send("Got a delete request at /user !!!")
-})
+// Replaced app.use() with app.get() for handling specific routes.
+// app.use() applies to all HTTP methods and routes that match the path.
+// ? makes the preceding character or group optional.
+// * allows the preceding character or group to repeat zero or more times.
+// + indicates that the preceding character or group can be repeated one or more times.
+// /a/ this route matches any path containing the letter 'a' anywhere in the URL.
 
 
+app.get( "/ab?cd/", (req , res) => {
+    res.send("Hello himanshu!! from /ca+b/ routes by devtinder server")
+})
 
-app.listen(7777, () => {
-    console.log("The server is listening on port 7777 successfully!!!");
+app.get( "/ab*cd/", (req , res) => {
+    res.send("Hello himanshu!! from /ca+b/ routes by devtinder server")    
+})
+
+
+app.get( "/ab+cd/", (req , res) => {
+    res.send("Hello himanshu!! from /ca+b/ routes by devtinder server")    
+})
+
+app.get(/a/ , (req , res) => {
+    res.send("Hello himanshu!! from /a/ routes by devtinder server")    
+})
+
+app.listen(3000, () => {
+    console.log("The server is listening on port 3000 successfully!!!");
 });
