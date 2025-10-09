f598c27ed8279d8be26fb9c1ded9f2505fb86026
himanshu-mawari <mawrihimanshu83@gmail.com>
2025-10-09 14:47:03 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,31 +1,34 @@
 const express = require("express");
 const app = express();
+const ports = 3000;
 
-// Replaced app.use() with app.get() for handling specific routes.
-// app.use() applies to all HTTP methods and routes that match the path.
-// ? makes the preceding character or group optional.
-// * allows the preceding character or group to repeat zero or more times.
-// + indicates that the preceding character or group can be repeated one or more times.
-// /a/ this route matches any path containing the letter 'a' anywhere in the URL.
+app.get(
+    "/user" , 
+    (req , res , next) =>{
+       console.log("handling user routes");
+       next();
+    } , 
+    (req , res , next) =>{
+        console.log("handling the route user 1");
+        // res.send("Ist response")
+        next()
+    } , 
+    (req , res , next) =>{
+        console.log("handling the route user 2");
+        // res.send("IInd response")
+        next()
+    } , 
+    (req , res , next) =>{
+        console.log("handling the route user 3");
+        // res.send("IIIrd response")
+        next()
+    } , 
+    (req , res , next) =>{
+        console.log("handling the route user 4");
+        res.send("IVth response")
+    }
+);
 
-
-app.get( "/ab?cd/", (req , res) => {
-    res.send("Hello himanshu!! from /ca+b/ routes by devtinder server")
+app.listen(ports , () => {
+    console.log("The server is listening on port 7777 successfully!!!");
 })
-
-app.get( "/ab*cd/", (req , res) => {
-    res.send("Hello himanshu!! from /ca+b/ routes by devtinder server")    
-})
-
-
-app.get( "/ab+cd/", (req , res) => {
-    res.send("Hello himanshu!! from /ca+b/ routes by devtinder server")    
-})
-
-app.get(/a/ , (req , res) => {
-    res.send("Hello himanshu!! from /a/ routes by devtinder server")    
-})
-
-app.listen(3000, () => {
-    console.log("The server is listening on port 3000 successfully!!!");
-});
