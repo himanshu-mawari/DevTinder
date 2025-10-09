81f2b2d05688400b485896adf4de0ca1fd3de787
himanshu-mawari <mawrihimanshu83@gmail.com>
2025-10-09 15:41:32 +0530

    exploring about middlewares

--- src/app.js
+++ src/app.js
@@ -2,33 +2,34 @@ const express = require("express");
 const app = express();
 const ports = 3000;
 
-app.get(
-    "/user" , 
-    (req , res , next) =>{
-       console.log("handling user routes");
-       next();
-    } , 
-    (req , res , next) =>{
-        console.log("handling the route user 1");
-        // res.send("Ist response")
-        next()
-    } , 
-    (req , res , next) =>{
-        console.log("handling the route user 2");
-        // res.send("IInd response")
-        next()
-    } , 
-    (req , res , next) =>{
-        console.log("handling the route user 3");
-        // res.send("IIIrd response")
-        next()
-    } , 
-    (req , res , next) =>{
-        console.log("handling the route user 4");
-        res.send("IVth response")
+// Handling routes with next()
+// Middleware functions can be used to process requests before they reach the route handler.
+
+
+app.use("/admin" , (req , res , next) =>{
+    let token = "hm5056";  // Simulated auth token for protected routes
+    if( token !== "hm5056"){
+        res.status(401).send("Permission denied. You are not authorized to access this resource.");
+    } else{
+        next();
     }
-);
+});
+
+app.get("/user" , (req , res , next) =>{
+    console.log("User middleware is running");
+    res.send("All users data access by admin");
+});
+
+// Auth middlewares for all /admin requests
+app.get("/admin/getAllUser" , (req , res , next) =>{
+    console.log("Admin middleware is running!!")
+    res.send("Hello from Admin get all user data!!");
+});
+
+app.get("/admin/deleteUser" , (req , res , next) =>{
+    res.send("Hello from Admin delete user of ID 1!!");
+});
 
 app.listen(ports , () => {
-    console.log("The server is listening on port 7777 successfully!!!");
+    console.log(`The server is listening on port ${ports} successfully!!!`);
 })
