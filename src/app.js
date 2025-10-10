2c7c64bc320b983b3547576fffe5a5e47e4144c5
himanshu-mawari <mawrihimanshu83@gmail.com>
2025-10-10 17:14:27 +0530

    wildcard error-handling

--- src/app.js
+++ src/app.js
@@ -3,29 +3,23 @@ const { adminAuth , userAuth } = require("./middlewares/auth")
 const app = express();
 const ports = 3000;
 
-// Handling routes with next()
-// Middleware functions can be used to process requests before they reach the route handler.
+// Error handling.
 
-app.use("/user" , userAuth)
-
-app.use("/admin" , adminAuth)
-
-app.get("/user/login" , (req , res) => {
-    res.send("User login!!")
-})
-
-app.get("/user/register" , (req , res) => {
-    res.send("User register");
+app.get("/userRegister" , (req , res , next) => {
+    // try{
+        throw new Error("Error occur")
+        res.send("User registered!!");
+    // } catch(err) {
+       
+    // }
 });
 
-app.get("/admin/getAllUser" , (req , res , next) => {
-    const user = JSON.stringify(req.query.id)
-    res.send(`${user} you dont have access these routes please exit or face the consiquences!!!`);
-});
-
-app.get("/admin/addAUser" , (req , res , next) =>{
-    res.send("This from the admin add a user with id : 1!!");
+app.use("/" , (err , req , res , next) =>{
+    if(err){
+        res.status(501).send("Something wrong!!")
+    }
 })
+
 
 
 
