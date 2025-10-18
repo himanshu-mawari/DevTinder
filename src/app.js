c6b6abfccef052ddc1462440c8181b85cfbf3dbf
himanshu-mawari <mawrihimanshu83@gmail.com>
2025-10-18 12:06:27 +0530

    validate user updates

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.get("/user", async (req, res) => {
     try {
         const users = await User.find({});
         if (!users) res.status(401).send("No user found");
-        else  res.send(users);
+        else res.send(users);
     } catch (err) {
         console.log("Error occur : " + err.message)
     }
@@ -31,47 +31,67 @@ app.get("/user", async (req, res) => {
 // API GET/user - to get user by their id.
 app.get("/user/id", async (req, res) => {
     const userId = req.body.userId;
-    try{
+    try {
         const users = await User.findById(userId);
-        if(!users) res.status(402).send("No user found");
+        if (!users) res.status(402).send("No user found");
         else res.send(users);
-    } catch(err) {
+    } catch (err) {
         res.status(402).send("Error occur : " + err.message);
-   }
+    }
 })
 
 // API GET/user - to get user by their gmail.
-app.get("/user/email" , async (req , res) => {
-    try{
+app.get("/user/email", async (req, res) => {
+    try {
         const emailId = req.body.emailId;
-        const user = await User.find({emailId : emailId});
-        if(!user) res.status(401).send("No user found wiht the provided email!");
+        const user = await User.find({ emailId: emailId });
+        if (!user) res.status(401).send("No user found wiht the provided email!");
         else res.send(user);
-    } catch(err) {
+    } catch (err) {
         res.status(402).send("Error occur :" + err.message);
     }
 })
 
 // API DELETE/user - to remove the user by their id
-app.delete("/user" , async (req , res) => {
-    try{
+app.delete("/user", async (req, res) => {
+    try {
         const userId = req.body.userId;
         await User.findByIdAndDelete(userId);
         res.send("User deleted successfully");
-    } catch(err) {
+    } catch (err) {
         res.status(402).send("Error occur :" + err.message);
     }
 });
 
-// API PATCH/user - to update the user specific field
-app.patch("/user" , async (req , res) => {
-    try{
-        const userId = req.body.userId;
+// API PATCH/user - to update the user specific field with some validation for certain thing you can updates.
+app.patch("/user/:userId", async (req, res) => {
+
+    try {
+        const userId = req.params.userId;
         const data = req.body;
-        await User.findByIdAndUpdate(userId , data) ;
+
+        const ALLOWED_UPDATES = [
+            "profilePicture",
+            "firstName",
+            "lastName",
+            "bio",
+            "skills",
+            "password",
+            "age",
+        ]
+
+        const isUpdateAllowed = Object.keys(data).every(k =>
+            ALLOWED_UPDATES.includes(k)
+        )
+
+        if (!isUpdateAllowed) {
+           res.status(400).send("Invalid updates! Sorry, you can't update certain fields")
+        }
+
+        await User.findByIdAndUpdate(userId, data , { runValidation : true});
         res.send("User updated successfully");
-    } catch(err) {
-        res.status(402).send("Error occur :" + err.message);
+    } catch (err) {
+        res.status(400).send("Error occur: " + err.message);
     }
 })
 
