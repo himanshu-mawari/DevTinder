7819aeec15a7ea9fb3d6b510677497edd0ad4ca4
himanshu-mawari <mawrihimanshu83@gmail.com>
2026-08-04 10:33:26 +0530

    feat: add fetch chat message history route

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -2,6 +2,8 @@ const express = require("express");
 const userAuth = require("../middlewares/auth");
 const Chat = require("../models/chat");
 const createError = require("../helpers/createError");
+const mongoose = require("mongoose");
+const Message = require("../models/message");
 
 const chatRouter = express.Router();
 
@@ -16,6 +18,35 @@ chatRouter.get("/", userAuth, async (req, res, next) => {
     res.json({
       message: "Successfully fetch chats",
       chats,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+chatRouter.get("/:chatId/messages", userAuth, async (req, res, next) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const { chatId } = req.params;
+
+    const chat = await Chat.findById(chatId);
+    if (!chat) {
+      return next(createError(404, "Chat not found"));
+    }
+    const isLoggedInUserParticipate =
+      chat.participants.includes(loggedInUserId);
+    if (!isLoggedInUserParticipate) {
+      return next(createError(403, "You are not a participant in this chat"));
+    }
+
+    const messages = await Message.find({ chatId: chat._id }).populate(
+      "senderId",
+      "firstName lastName",
+    );
+
+    res.json({
+      message: "Successfully fetched all messages",
+      messages,
     });
   } catch (err) {
     next(err);
